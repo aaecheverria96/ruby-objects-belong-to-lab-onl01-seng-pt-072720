713e1e99660f727dd343b50fc3cd170dac1333e7
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-09-17 16:24:29 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,9 @@
-class Post 
+class Post
 
-  attr_accessor :title 
+  attr_accessor :title
 
-  def initialize(title) 
-    @title = title 
-end 
+  def initialize(title)
+    @title = title
+end
 
 end 
